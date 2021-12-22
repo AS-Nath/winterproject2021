698a6497a6a90f4a5f24ee8df283905997d2a1b1
AS-Nath <aditya.nath.tech@gmail.com>
2021-12-22 18:24:15 +0530

    First program done

--- Program1.java
+++ Program1.java
@@ -1,5 +1,32 @@
+//WAP to accept a number and print prime numbers in a range.
 import java.util.*;
-class Program1
-{
-    
+
+class Program1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the lower limit: ");
+        int lower = sc.nextInt();
+        System.out.println("Enter the upper limit: ");
+        int upper = sc.nextInt();
+
+        System.out.println();
+
+        for (int i = lower; i <= upper; i++) {
+            boolean isPrime = Prime(i);
+            if (isPrime == true) {
+                System.out.println(i);
+            }
+        }
+        sc.close();
+    }
+    public static boolean Prime(int i) {
+        boolean return_val = true;
+        for (int c = 2; c < i; c++) {
+            if (i % c == 0) {
+                return_val = false; 
+            }
+        }
+        return return_val;
+    }
+
 }
